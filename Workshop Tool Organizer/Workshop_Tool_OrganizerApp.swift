e4c2b21f9abf622af16c4a8d6b02f57af1004ac3
dsv <zxcv>
2024-10-03 00:02:02 +0500

    Initial Commit

---
+++ Workshop Tool Organizer/Workshop_Tool_OrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workshop_Tool_OrganizerApp.swift
+//  Workshop Tool Organizer
+//
+//  Created by Muhammad Rizwan on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Workshop_Tool_OrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
